f0984f8c12e65b5416049fbf3aebd36deffbb8b2
dy0yb <15639927207@sina.cn>
2019-10-09 16:14:53 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
+
+import './lib/MUI/css/mui.min.css'
+
+import app from './App.vue'
+
+var vm = new Vue({
+    el:'#app',
+    render: c=> c(app)
+});
