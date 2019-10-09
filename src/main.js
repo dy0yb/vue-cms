0876ca850cad0433f59e1527336c3ea8d23f25f0
dy0yb <15639927207@sina.cn>
2019-10-09 17:35:17 +0800

    配置底部图标

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { Header } from 'mint-ui';
 Vue.component(Header.name, Header);
 
 import './lib/MUI/css/mui.min.css'
+import './lib/MUI/css/icons-extra.css'
+
 
 import app from './App.vue'
 
